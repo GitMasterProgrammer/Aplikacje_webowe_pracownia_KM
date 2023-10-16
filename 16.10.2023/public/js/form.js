262b3d4f264100af1f8e798227dc279812ff0a07
GitMasterProgrammer <118828179+GitMasterProgrammer@users.noreply.github.com>
2023-10-16 14:38:18 +0200

    Add files via upload

---
+++ 16.10.2023/public/js/form.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("Nie można wysłac formularza")
+});
